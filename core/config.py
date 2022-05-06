68dcf324c51451eb20dff3e8b75da2c715f25cb2
Grin <asusgrin@gmail.com>
2022-05-07 00:25:06 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,8 +2,7 @@ from starlette.config import Config
 config = Config(".env")
 # Переменная окружения
 DATABASE_URL = config("DATABASE_URL", cast=str, default="")
-#export DATABASE_URL="mysql+mysqlconnector://bot:qwer1234t5@localhost:3308/bot_db"
 TELEGRAM_TOCKEN = config("TELEGRAM_TOCKEN", cast=str, default="")
-#export TELEGRAM_TOCKEN="5171909645:AAG8HefH3P9iSSk4eMec70jBde24Vxtb3b8"
+
 
 
